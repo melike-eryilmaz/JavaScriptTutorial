b87de6b826bb94d488f78210eb2496c42bfd51f6
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2021-03-26 13:28:36 +0300

    JavaScript variables hakkında bilgiler eklendi.

---
+++ 2-lessonTwo.js
@@ -0,0 +1,18 @@
+/**
+ *Değişkenler
+	Javascript de değişkenler var keyword ile tanımlanır. (variable-var)
+	Değişken adı rakam olamaz,değiken adı rakamla başlayamaz ama rakamla bitebilir.
+	_ değişken tanımlarında kullanılabilri
+	$ değiken tanımlarında kullanılabilir.
+	Fonksiyon içerisinde tanımlanan bir değişken sadece fonksiyon içerisinde kalır.
+	Fonksiyon içerisinde var ile değişken tanımlamamız gereklidir.Tanımlamazsak global olarak algılanır.
+	
+	
+ */
+
+ isim = "Melike";
+
+ function getName(){
+     var isim = "Merve";
+    console.log(isim)
+ }
